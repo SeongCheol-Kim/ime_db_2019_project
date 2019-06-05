966c61706da02ed6ca8d61d4a3a42ecb3d8d813d
SeongCheol Kim <sckim219@gmail.com>
2019-06-06 01:00:08 +0900

    update table

--- sql_model.py
+++ sql_model.py
@@ -1,6 +1,6 @@
 import os
 import json
-from sqlalchemy import Column, String, Integer, Boolean, Date, Table, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, Boolean, Date, Table, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 from sqlalchemy import create_engine
@@ -18,7 +18,7 @@ engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(
     secrets['LOCAL_USER_PASSWORD'],
     secrets['LOCAL_USER_URL'],
     secrets['RDS_PORT'],
-    secrets['RDS_DATABASE']
+    secrets['DATABASE_NAME']
 ))
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
@@ -26,46 +26,54 @@ Base = declarative_base()
 
 info_association = Table(
     'total_information', Base.metadata,
-    Column('RegionID', String(32), ForeignKey('regions.Code')),
-    Column('CategoryID', String(32), ForeignKey('categories.Code')),
-    Column('InfoID', Integer, ForeignKey('informations.ID'))
+    Column('RegionID', Integer, ForeignKey('regions.id')),
+    Column('CategoryID', Integer, ForeignKey('facilities.id')),
+    Column('ObjectID', Integer, ForeignKey('informations.id'))
 )
 
 
 class Region(Base):                                  # 지역 구분
     __tablename__ = 'regions'
 
-    Code = Column(String(32), primary_key=True)          # 지구구분코드
-    Latitude = Column(Integer)
-    Longitude = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    code = Column(String(32))          # 지구구분코드
+    name = Column(String(64))
 
-    def __init__(self, Code, Latitude, Longitude):
+    def __init__(self, code, name):
 
-        self.Code = Code
-        self.Latitude = Latitude
-        self.Longitude = Longitude
+        self.code = code
+        self.name = name
 
 
-class Category(Base):                                # 종목 구분
-    __tablename__ = 'categories'
+class Facility(Base):                                # 종목 구분
+    __tablename__ = 'facilities'
 
-    Code = Column(String(32), primary_key=True)      # 지형지물 코드
+    id = Column(Integer, primary_key=True)
+    code = Column(String(32))      # 지형지물 코드
     name = Column(String(32))
 
-    def __init__(self, Code, name):
-        self.Code = Code
+    def __init__(self, code, name):
+        self.code = code
         self.name = name
 
 
 class Information(Base):                            # 종합 정보
     __tablename__ = 'informations'
 
-    ID = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    region_id = Column(Integer, ForeignKey('regions.id'), nullable=False)
+    facilities_id = Column(Integer, ForeignKey('facilities.id'), nullable=False)
+    category = Column(String(32))
     name = Column(String(64))
-    TEL = Column(String(64))
+    tel = Column(String(64))
     fare = Column(String(256))
+    latitude = Column(Float)
+    longitude = Column(Float)
 
-    def __init__(self, name, TEL, fare):
+    def __init__(self, category, name, tel, fare, latitude, longitude):
+        self.category = category
         self.name = name
-        self.TEL = TEL
+        self.tel = tel
         self.fare = fare
+        self.latitude = latitude
+        self.longitude = longitude
