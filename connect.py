e122af871df0d4f88dec9fa9113a877b4296ba3e
SeongCheol Kim <sckim219@gmail.com>
2019-06-06 05:21:30 +0900

    add session

---
+++ connect.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+ROOT_DIR = os.getcwd()
+SECRET_DIR = os.path.join(ROOT_DIR, '.secrets')
+secrets = json.load(open(os.path.join(SECRET_DIR, "secrets.json")))
+
+Base = declarative_base()
+
+
+def connect_database():
+    engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+        secrets['LOCAL_USER_ID'],
+        secrets['LOCAL_USER_PASSWORD'],
+        secrets['LOCAL_USER_URL'],
+        secrets['RDS_PORT'],
+        secrets['DATABASE_NAME']
+    ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    return session
